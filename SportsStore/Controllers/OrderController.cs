6a361101973f90895f1434617d5092fc77e5a99d
Vladimir Jovanovic <vladimirjo@protonmail.com>
2022-06-08 06:39:22 +0200

    create order page

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers;
+
+public class OrderController : Controller
+{
+    public ViewResult Checkout() => View(new Order());
+}
